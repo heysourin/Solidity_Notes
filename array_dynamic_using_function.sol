42aee5063f4a3c56cdc8d3823f590d57ff61a1f5
Sourin <98945276+heysourin@users.noreply.github.com>
2022-06-27 21:09:42 +0530

    Dynamic Size Array

--- array_dynamic_using_function.sol
+++ array_dynamic_using_function.sol
@@ -5,10 +5,14 @@ pragma solidity >=0.5.0 <0.9.0;
 contract hlo2{
    uint[] public id;
 
+
+//to assign values in the array
    function Insert(uint indx) public{
        id.push(indx);
    }
 
+
+//To check inserted(pushed) number is at 'which'th index
    function get(uint cntnt) public view returns(uint){
        for(uint i=0; i<id.length; i++){
            if(cntnt==id[i]){
@@ -17,10 +21,14 @@ contract hlo2{
        }
    }
 
+
+//to print the array
    function getAll() public view returns (uint[] memory){
         return id;
    }
    
+
+//to get the length
    function length() public view returns(uint){
        return id.length;
    }

19747063b73bf87f602b5f454971ca77e78fbcbe
Sourin <98945276+heysourin@users.noreply.github.com>
2022-07-12 13:45:25 +0530

    Array Summary

---
+++ Array_all.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.8;
+
+contract demo99{
+
+// ***********Fixed Sized Array*************
+    // uint[3] public arr1;
+
+    // function dataOfArr() public returns(int[5] memory, uint[3] memory) {
+    //     int[5] memory data1 = [int(50), -63, 77, -28, 90];
+    //     arr1=[uint(11),21,31];
+
+    //     return (data1, arr1);// see the whole output in the terminal
+    // }
+
+
+// ***********Dynamic Sized Array*************
+    // uint[] public arr2;
+    
+    // function arr2Add(uint _val) public returns(uint[] memory) {
+    //     arr2.push(_val);
+    //     return arr2;// see the whole output in the terminal
+    // }
+
+    // function seeArr2() public view returns(uint[] memory){
+    //     return arr2;
+    // }
+
+    // function array_element() public payable returns (uint){  
+    //     return arr2[2];// see the output in the terminal
+    // }  
+
+    // function popArr() public {
+    //     arr2.pop();
+    // }
+}
+
+

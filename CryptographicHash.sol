697071650a8f275b74cc7f9a5d8cd43940ea8f7c
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-07 15:19:26 +0530

    Cryptographic Hash

---
+++ CryptographicHash.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.8;
+
+contract demo{
+
+    function hashKeccak256(uint _x, string memory name, address _add) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(_x, name, _add));
+    }
+
+    function hashSha256(uint _x, string memory name, address _add) public pure returns(bytes32){
+        return sha256(abi.encodePacked(_x, name, _add));
+        
+    }
+
+    function hashRipemd160(uint _x, string memory name, address _add) public pure returns(bytes32){
+        return ripemd160(abi.encodePacked(_x, name, _add));
+        
+    }
+}
